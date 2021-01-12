a885f6e8a3d18b0bf5b653d9261ed086c0ef1261
sakutaro.hagiwara <kzkc.ggg@gmail.com>
2021-01-12 09:27:12 +0000

    complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :prefecture
   
-  validates :content, presence: true, length: { minimum:5,maximum: 30 }
+  validates :content, presence: true, length: { maximum: 20 }
 end
