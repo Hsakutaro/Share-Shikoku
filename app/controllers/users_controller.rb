fe14dff94c04738b242cbd03c3cb1e57dbd2d6f0
sakutaro.hagiwara <kzkc.ggg@gmail.com>
2021-01-14 05:02:24 +0000

    revise 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,10 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to root_url
-    else
+      redirect_to  root_path
+      else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
     end
